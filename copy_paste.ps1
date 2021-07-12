1573b5b4615f451a1e37a64ea43ae3890dfe6f68
Trey <stockmankenne@ad.garmin.com>
2021-07-12 16:29:31 -0500

    Uploading progress

---
+++ copy_paste.ps1
@@ -0,0 +1,7 @@
+# This script is designed to copy files/folders from one directory to another.
+
+Write-Host "***THIS SCRIPT WILL NOT WORK IF YOU DO NOT RUN POWERSHELL AS ADMINISTRATOR"
+$uPath = Read-Host -Prompt "What is the file path you wish to copy?"
+$uDest = Read-Host -Prompt "Where would you like this copied to?"
+
+Copy-Item -Path $uPath* -Destination $uDest* -PassThru -Recurse
